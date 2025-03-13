2d57202971061b0a8b92a9263b3a3ed69cd6e8ba
Gmontes <ghmontes92@gmail.com>
2025-03-13 03:56:49 -0300

    Se agrego animacion completa con Framer Motion

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -1,14 +1,23 @@
 import { useEffect, useReducer, useRef } from "react";
-import { initTareas, tareasInitialState, tareasReducer } from "../reducers/tareasReducer";
+import {
+  initTareas,
+  tareasInitialState,
+  tareasReducer,
+} from "../reducers/tareasReducer";
 import { TYPES } from "../actions/tareasActions";
 import { v4 as uuidv4 } from "uuid";
+import { motion } from "framer-motion";
 import Tareas from "./Tareas";
 
 const ListaTareas = () => {
   const inputRef = useRef();
 
-  const [tareas, dispatch] = useReducer(tareasReducer, tareasInitialState,initTareas);
-  
+  const [tareas, dispatch] = useReducer(
+    tareasReducer,
+    tareasInitialState,
+    initTareas
+  );
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (inputRef.current.value.length <= 1) return;
@@ -22,44 +31,68 @@ const ListaTareas = () => {
     inputRef.current.value = "";
   };
 
-  useEffect(()=>{
-    localStorage.setItem('tareas',JSON.stringify(tareas))
-  },[tareas])
+  useEffect(() => {
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+  }, [tareas]);
 
-  const contadorTareas = tareas.length
-  const tareasPendientes = tareas.filter(tarea => !tarea.done).length
+  const contadorTareas = tareas.length;
+  const tareasPendientes = tareas.filter((tarea) => !tarea.done).length;
+  const tareasCompletas = tareas.filter((tarea) => tarea.done).length;
 
   return (
-    <div className="grid justify-center h-screen text-slate-100">
+    //<AnimatePresence>
+    <div className="grid justify-center h-screen text-slate-100 min-w-80 mx-2">
       <div className="content-center">
+        <motion.div 
+        initial={{
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{ duration: 1.5, type: "spring" }}
+        className="border border-blue-950/50 rounded-2xl p-6 shadow-md shadow-blue-500/50">
+          <h1 className="text-center text-3xl m-4">
+            Lista de tareas
+          </h1>
+          <div className="flex justify-between my-6 text-sm">
+            
+          <h3 className=" border-b-3  border-cyan-600/70  ">
+              N° Tareas: {contadorTareas}
+            </h3>
+            <h3 className=" border-b-3  border-green-600/70  ">
+              Completas: {tareasCompletas}
+            </h3>
+            <h3 className=" border-b-3  border-orange-600/70 ">
+              Pendientes: {tareasPendientes}
+            </h3>
+          </div>
+          <form onSubmit={handleSubmit} className="text-lg">
+            <label>Tarea: </label>
+            <input
+              className="bg-slate-200 text-slate-800 rounded-lg px-2 max-w-44"
+              type="text"
+              name="title"
+              ref={inputRef}
+              placeholder="Escribre una tarea.."
+            />
+            <motion.input
+              className="bg-sky-900 p-0.5 border-2  border-sky-700 m-2 cursor-pointer rounded-lg"
+              type="submit"
+              value="Agregar"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            />
+          </form>
+        </motion.div>
 
-      <div className="border border-blue-950/50 rounded-2xl p-6 shadow-md shadow-blue-500/50">
-      <h1 className="text-center text-3xl m-4">Lista de tareas</h1>
-      <div className="flex justify-between my-6">
-        <h3 className=" border-b-3  border-cyan-600 ">N° Tareas: {contadorTareas}</h3>
-        <h3 className=" border-b-3  border-orange-600">Pendientes: {tareasPendientes}</h3>
-      </div>
-      <form onSubmit={handleSubmit} className="text-lg">
-        <label className="mr-2">Tarea:</label>
-        <input
-          className="bg-slate-200 text-slate-800 rounded-lg px-2"
-          type="text"
-          name="title"
-          ref={inputRef}
-          placeholder="Escribre una tarea.."
-        />
-        <input 
-          className="bg-sky-900 p-0.5 border-2 border-sky-700 ml-2 cursor-pointer rounded-lg"
-          type="submit"
-          value="Agregar"
-           />
-      </form>
-      </div>
-      
-      <Tareas tareas={tareas} dispatch={dispatch} />
+        <Tareas tareas={tareas} dispatch={dispatch} />
 
       </div>
     </div>
+    //    </AnimatePresence>
   );
 };
 
--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -1,27 +1,70 @@
-
-import { TYPES } from '../actions/tareasActions';
+import { TYPES } from "../actions/tareasActions";
 import { FaTrashAlt } from "react-icons/fa";
 import { MdPlaylistAddCheckCircle } from "react-icons/md";
+import { motion,AnimatePresence} from "framer-motion";
 
 
 const Tareas = ({ tareas, dispatch }) => {
-    return (
-        <>
-            { tareas.map((tarea) => (
-                <div key={tarea.id} className={tarea.done?`border border-green-600/50 rounded-2xl my-6 p-2 shadow-md shadow-green-600/50 line-through`: `border border-orange-600/50 rounded-2xl my-6 p-2 shadow-md shadow-orange-600/50 `}>
-                    <p className='text-center m-4 p-1'>{tarea.title}</p>
-                    <div className='flex justify-around'>
-                        <button className='text-2xl cursor-pointer' onClick={() => dispatch({ type: TYPES.COMPLETADA, payload: tarea.id })}><MdPlaylistAddCheckCircle /></button>
-                        <button className='text-xl cursor-pointer' onClick={() => dispatch({ type: TYPES.ELIMINAR, payload: tarea.id })}><FaTrashAlt /></button>
 
-                    </div>
-                </div>
-                
-            ))}
 
-            
-        </>
-    )
-}
+  return (
+    <>
+       <AnimatePresence mode="">
+      {tareas.map((tarea) => (
+        
+        <motion.div
+          key={tarea.id}
+          className={
+            tarea.done
+              ? `border border-green-600/50 rounded-2xl my-6 p-2 shadow-md shadow-green-600/50 line-through transition duration-500`
+              : `border border-orange-600/50 rounded-2xl my-6 p-2 shadow-md shadow-orange-600/50 `
+          }
+          initial={{
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{ duration: 1, type: "spring" }}
+          exit={{
+            opacity:0,
+            scale:0
+          }}
+          layoutId={tarea.id}
+  
 
-export default Tareas
+        >
+          <p className="text-center m-4 p-1">{tarea.title}</p>
+          <div className="flex justify-around">
+            <motion.button
+              className="text-2xl cursor-pointer"
+              onClick={() =>
+                dispatch({ type: TYPES.COMPLETADA, payload: tarea.id })
+              }
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <MdPlaylistAddCheckCircle />
+            </motion.button>
+            <motion.button
+              className="text-xl cursor-pointer"
+              onClick={() =>
+                dispatch({ type: TYPES.ELIMINAR, payload: tarea.id })
+              }
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <FaTrashAlt />
+            </motion.button>
+          </div>
+        </motion.div>
+        
+      ))}
+      </AnimatePresence> 
+    </>
+  );
+};
+
+export default Tareas;
